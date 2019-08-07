58d296f58232faf84234890212f7fc4be23dd4c0
MaybeUreRight <211592649@qq.com>
2019-08-07 22:00:31 +0800

    Update AsyncZipTask.java
    添加回调，确定用户在解压加密的压缩包的过程中因为解压密码不正确而抓不到抛出的ZipException

--- src/main/java/net/lingala/zip4j/tasks/AsyncZipTask.java
+++ src/main/java/net/lingala/zip4j/tasks/AsyncZipTask.java
@@ -1,5 +1,6 @@
 package net.lingala.zip4j.tasks;
 
+import net.lingala.zip4j.callback.UncatchException;
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.progress.ProgressMonitor;
 
@@ -8,61 +9,85 @@ import java.util.concurrent.Executors;
 
 public abstract class AsyncZipTask<T> {
 
-  private ProgressMonitor progressMonitor;
-  private boolean runInThread;
+    private ProgressMonitor progressMonitor;
+    private boolean runInThread;
 
-  public AsyncZipTask(ProgressMonitor progressMonitor, boolean runInThread) {
-    this.progressMonitor = progressMonitor;
-    this.runInThread = runInThread;
-  }
+    public AsyncZipTask(ProgressMonitor progressMonitor, boolean runInThread) {
+        this.progressMonitor = progressMonitor;
+        this.runInThread = runInThread;
+    }
 
-  public void execute(T taskParameters) throws ZipException {
-    progressMonitor.fullReset();
-    progressMonitor.setState(ProgressMonitor.State.BUSY);
-    progressMonitor.setCurrentTask(getTask());
+    public void execute(T taskParameters) throws ZipException {
+        progressMonitor.fullReset();
+        progressMonitor.setState(ProgressMonitor.State.BUSY);
+        progressMonitor.setCurrentTask(getTask());
 
-    if (runInThread) {
-      long totalWorkToBeDone = calculateTotalWork(taskParameters);
-      progressMonitor.setTotalWork(totalWorkToBeDone);
+        if (runInThread) {
+            long totalWorkToBeDone = calculateTotalWork(taskParameters);
+            progressMonitor.setTotalWork(totalWorkToBeDone);
 
-      Executors.newSingleThreadExecutor().execute(() -> {
-        try {
-          performTaskWithErrorHandling(taskParameters, progressMonitor);
-        } catch (ZipException e) {
-          //Do nothing. Exception will be passed through progress monitor
+            Executors.newSingleThreadExecutor().execute(() -> {
+                try {
+                    performTaskWithErrorHandling(taskParameters, progressMonitor);
+                } catch (ZipException e) {
+                    //Do nothing. Exception will be passed through progress monitor
+                }
+            });
+        } else {
+            performTaskWithErrorHandling(taskParameters, progressMonitor);
         }
-      });
-    } else {
-      performTaskWithErrorHandling(taskParameters, progressMonitor);
-    }
-  }
-
-  private void performTaskWithErrorHandling(T taskParameters, ProgressMonitor progressMonitor) throws ZipException {
-    try {
-      executeTask(taskParameters, progressMonitor);
-      progressMonitor.endProgressMonitor();
-    } catch (ZipException e) {
-      progressMonitor.endProgressMonitor(e);
-      throw e;
-    } catch (Exception e) {
-      progressMonitor.endProgressMonitor(e);
-      throw new ZipException(e);
-    }
-  }
-
-  protected void verifyIfTaskIsCancelled() throws ZipException {
-    if (!progressMonitor.isCancelAllTasks()) {
-      return;
     }
 
-    progressMonitor.setResult(ProgressMonitor.Result.CANCELLED);
-    progressMonitor.setState(ProgressMonitor.State.READY);
-    throw new ZipException("Task cancelled", ZipException.Type.TASK_CANCELLED_EXCEPTION);
-  }
+    public void execute(T taskParameters, UncatchException uncatchException) throws ZipException {
+        progressMonitor.fullReset();
+        progressMonitor.setState(ProgressMonitor.State.BUSY);
+        progressMonitor.setCurrentTask(getTask());
 
-  protected abstract void executeTask(T taskParameters, ProgressMonitor progressMonitor) throws IOException;
+        if (runInThread) {
+            long totalWorkToBeDone = calculateTotalWork(taskParameters);
+            progressMonitor.setTotalWork(totalWorkToBeDone);
 
-  protected abstract long calculateTotalWork(T taskParameters) throws ZipException;
+            Executors.newSingleThreadExecutor().execute(() -> {
+                try {
+                    performTaskWithErrorHandling(taskParameters, progressMonitor);
+                } catch (ZipException e) {
+                    //Do nothing. Exception will be passed through progress monitor
+                    uncatchException.wrongPassword(e);
+                }
+            });
+        } else {
+            performTaskWithErrorHandling(taskParameters, progressMonitor);
+        }
+    }
 
-  protected abstract ProgressMonitor.Task getTask();
+
+
+    private void performTaskWithErrorHandling(T taskParameters, ProgressMonitor progressMonitor) throws ZipException {
+        try {
+            executeTask(taskParameters, progressMonitor);
+            progressMonitor.endProgressMonitor();
+        } catch (ZipException e) {
+            progressMonitor.endProgressMonitor(e);
+            throw e;
+        } catch (Exception e) {
+            progressMonitor.endProgressMonitor(e);
+            throw new ZipException(e);
+        }
+    }
+
+    protected void verifyIfTaskIsCancelled() throws ZipException {
+        if (!progressMonitor.isCancelAllTasks()) {
+            return;
+        }
+
+        progressMonitor.setResult(ProgressMonitor.Result.CANCELLED);
+        progressMonitor.setState(ProgressMonitor.State.READY);
+        throw new ZipException("Task cancelled", ZipException.Type.TASK_CANCELLED_EXCEPTION);
+    }
+
+    protected abstract void executeTask(T taskParameters, ProgressMonitor progressMonitor) throws IOException;
+
+    protected abstract long calculateTotalWork(T taskParameters) throws ZipException;
+
+    protected abstract ProgressMonitor.Task getTask();
 }
