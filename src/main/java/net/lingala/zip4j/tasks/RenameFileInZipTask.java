ac816aaae9666ae9fd0f708f4183e8cf616e98d3
LeeYoung624 <liyiang@hotmail.com>
2019-10-09 14:29:34 +0800

    deal with the case when renaming the directory

--- src/main/java/net/lingala/zip4j/tasks/RenameFileInZipTask.java
+++ src/main/java/net/lingala/zip4j/tasks/RenameFileInZipTask.java
@@ -1,7 +1,6 @@
 package net.lingala.zip4j.tasks;
 
 import net.lingala.zip4j.exception.ZipException;
-import net.lingala.zip4j.headers.FileHeaderFactory;
 import net.lingala.zip4j.headers.HeaderReader;
 import net.lingala.zip4j.headers.HeaderWriter;
 import net.lingala.zip4j.io.outputstream.SplitOutputStream;
@@ -19,13 +18,16 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.Channels;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static net.lingala.zip4j.headers.HeaderUtil.getIndexOfFileHeader;
+import static net.lingala.zip4j.headers.HeaderUtil.getFileHeader;
 
 public class RenameFileInZipTask extends AbstractModifyFileTask<RenameFileInZipTaskParameters> {
   private ZipModel zipModel;
-  private FileHeaderFactory fileHeaderFactory = new FileHeaderFactory();
 
   public RenameFileInZipTask(ProgressMonitor progressMonitor, boolean runInThread, ZipModel zipModel) {
     super(progressMonitor, runInThread);
@@ -43,46 +45,107 @@ public class RenameFileInZipTask extends AbstractModifyFileTask<RenameFileInZipT
       return;
     }
 
+    if(checkIfNewNameIsUsed(taskParameters.newFileName)) {
+      throw new ZipException("The new file name already exists in the zip file.");
+    }
+
     File temporaryZipFile = getTemporaryFile(zipModel.getZipFile().getPath());
     boolean successFlag = false;
 
     try (SplitOutputStream outputStream = new SplitOutputStream(temporaryZipFile);
          RandomAccessFile inputStream = new RandomAccessFile(zipModel.getZipFile(),
                  RandomAccessFileMode.READ.getValue())){
+
       HeaderWriter headerWriter = new HeaderWriter();
-      HeaderReader headerReader = new HeaderReader();
-      int indexOfFileHeader = getIndexOfFileHeader(zipModel, taskParameters.fileHeader);
-      long offsetLocalFileHeader = getOffsetLocalFileHeader(taskParameters.fileHeader);
-      long offsetStartOfCentralDirectory = getOffsetOfStartOfCentralDirectory(zipModel);
-      // read the local file header
-      inputStream.seek(offsetLocalFileHeader);
-      LocalFileHeader localFileHeader = headerReader.readLocalFileHeader(Channels.newInputStream(inputStream.getChannel()), taskParameters.charset);
-      long offsetEndOfLocalFileHeader = getOffsetEndOfLocalFileHeader(offsetLocalFileHeader, localFileHeader);
+      if(!taskParameters.fileHeader.isDirectory()) {
+        Map<FileHeader, Long> localFileHeaderOffsetMap = new HashMap<>();
+        localFileHeaderOffsetMap.put(taskParameters.fileHeader, getOffsetLocalFileHeader(taskParameters.fileHeader));
 
-      int newFileNameLength = taskParameters.newFileName.getBytes(taskParameters.charset).length;
-      // calculate the diff of length in local file header, which is caused by the change of file name
-      int fileNameLengthDiff = newFileNameLength - localFileHeader.getFileNameLength();
-      localFileHeader.setFileName(taskParameters.newFileName);
-      localFileHeader.setFileNameLength(newFileNameLength);
+        writeLocalFileHeaderAndUpdateOffsets(taskParameters.fileHeader, null, localFileHeaderOffsetMap, true,
+                taskParameters.newFileName, taskParameters.charset, outputStream, inputStream, headerWriter, progressMonitor);
+      } else {
+        // if the directory is being renamed, the files in the directory should also be renamed
+        List<FileHeader> fileHeaders = zipModel.getCentralDirectory().getFileHeaders();
+        List<FileHeader> fileHeadersInDirectory = new ArrayList<>();
+        String oldName = taskParameters.fileHeader.getFileName();
 
-      if(indexOfFileHeader > 0) {
-        // copy the content before the changed local file header
-        FileUtils.copyFile(inputStream, outputStream, 0, offsetLocalFileHeader, progressMonitor);
+        Map<FileHeader, Long> localFileHeaderOffsetMap = new HashMap<>();
+        for(FileHeader fileHeader : fileHeaders) {
+          if(fileHeader.getFileName().startsWith(oldName)) {
+            fileHeadersInDirectory.add(fileHeader);
+            // store the local file header offset here as it may mutate after update
+            localFileHeaderOffsetMap.put(fileHeader, getOffsetLocalFileHeader(fileHeader));
+          }
+        }
+
+        for(int i = 0; i < fileHeadersInDirectory.size();i++) {
+          FileHeader fileHeader = fileHeadersInDirectory.get(i);
+          FileHeader nextFileHeader = null;
+          if(i < fileHeadersInDirectory.size() - 1) {
+            nextFileHeader = fileHeadersInDirectory.get(i + 1);
+          }
+
+          boolean isFirstFileHeader = i == 0 ? true : false;
+          String newFileName = fileHeader.getFileName().replaceFirst(oldName, taskParameters.newFileName);
+
+          writeLocalFileHeaderAndUpdateOffsets(fileHeader, nextFileHeader, localFileHeaderOffsetMap, isFirstFileHeader, newFileName,
+                  taskParameters.charset, outputStream, inputStream, headerWriter, progressMonitor);
+        }
       }
 
-      // write the new local file header with new file name
-      headerWriter.writeLocalFileHeader(zipModel, localFileHeader, outputStream, taskParameters.charset);
-
-      // copy the rest of the content in this file, and the data of other files
-      FileUtils.copyFile(inputStream, outputStream, offsetEndOfLocalFileHeader, offsetStartOfCentralDirectory, progressMonitor);
-
-      updateHeaders(outputStream, indexOfFileHeader, taskParameters.newFileName, newFileNameLength, fileNameLengthDiff);
       headerWriter.finalizeZipFile(zipModel, outputStream, taskParameters.charset);
-
       successFlag = true;
     } finally {
       cleanupFile(successFlag, zipModel.getZipFile(), temporaryZipFile);
     }
+  }
+
+  private void writeLocalFileHeaderAndUpdateOffsets(FileHeader fileHeader, FileHeader nextFileHeader, Map<FileHeader, Long> localFileHeaderOffsetMap, boolean isFirstHeader, String newFileName, Charset charset,
+                                                    SplitOutputStream outputStream, RandomAccessFile inputStream, HeaderWriter headerWriter, ProgressMonitor progressMonitor) throws IOException {
+    HeaderReader headerReader = new HeaderReader();
+    int indexOfFileHeader = getIndexOfFileHeader(zipModel, fileHeader);
+    long offsetLocalFileHeader = localFileHeaderOffsetMap.get(fileHeader);
+    inputStream.seek(offsetLocalFileHeader);
+    LocalFileHeader localFileHeader = headerReader.readLocalFileHeader(Channels.newInputStream(inputStream.getChannel()), charset);
+    long offsetEndOfLocalFileHeader = getOffsetEndOfLocalFileHeader(offsetLocalFileHeader, localFileHeader);
+
+    int newFileNameLength = newFileName.getBytes(charset).length;
+    // calculate the diff of length in local file header, which is caused by the change of file name
+    int fileNameLengthDiff = newFileNameLength - localFileHeader.getFileNameLength();
+    localFileHeader.setFileName(newFileName);
+    localFileHeader.setFileNameLength(newFileNameLength);
+
+    if(isFirstHeader && indexOfFileHeader > 0) {
+      // copy the content before the changed local file header
+      FileUtils.copyFile(inputStream, outputStream, 0, offsetLocalFileHeader, progressMonitor);
+    }
+
+    // write the new local file header with new file name
+    headerWriter.writeLocalFileHeader(zipModel, localFileHeader, outputStream, charset);
+
+    // copy the rest of the content in this file, and the data of other files
+    long writeEndOffset;
+    if(nextFileHeader == null) {
+      writeEndOffset = getOffsetOfStartOfCentralDirectory(zipModel);
+    } else {
+      writeEndOffset = localFileHeaderOffsetMap.get(nextFileHeader);
+    }
+    FileUtils.copyFile(inputStream, outputStream, offsetEndOfLocalFileHeader, writeEndOffset, progressMonitor);
+
+    if(nextFileHeader == null) {
+      // update the offset of start of central directory when finish writing all headers
+      updateEndOfCentralDirectoryRecord(outputStream);
+    }
+    updateHeaders(indexOfFileHeader, newFileName, newFileNameLength, fileNameLengthDiff);
+  }
+
+  private boolean checkIfNewNameIsUsed(String newFileName) throws ZipException {
+    FileHeader fileHeader = getFileHeader(zipModel, newFileName);
+    if(fileHeader != null) {
+      return true;
+    }
+
+    return false;
   }
 
   /**
@@ -113,21 +176,19 @@ public class RenameFileInZipTask extends AbstractModifyFileTask<RenameFileInZipT
    * the file name and file name length in central directory header should be changed accordingly,
    * the offset of the offset of start of central directory should be changed accordingly
    * the offset of the files after the changed file should be changed accordingly
-   * @param splitOutputStream
    * @param indexOfFileHeader
    * @param newFileName
    * @param newFileNameLength
    * @param fileNameLengthDiff
    * @throws IOException
    */
-  private void updateHeaders(SplitOutputStream splitOutputStream, int indexOfFileHeader, String newFileName,
+  private void updateHeaders(int indexOfFileHeader, String newFileName,
                              int newFileNameLength, int fileNameLengthDiff) throws IOException {
     FileHeader fileHeader = zipModel.getCentralDirectory().getFileHeaders().get(indexOfFileHeader);
     fileHeader.setFileName(newFileName);
     fileHeader.setFileNameLength(newFileNameLength);
     if(fileNameLengthDiff != 0) {
       // no need to update the offsets if the length of 2 file names are same
-      updateEndOfCentralDirectoryRecord(splitOutputStream);
       updateFileHeadersWithLocalHeaderOffsets(zipModel.getCentralDirectory().getFileHeaders(), fileNameLengthDiff, indexOfFileHeader);
     }
   }
