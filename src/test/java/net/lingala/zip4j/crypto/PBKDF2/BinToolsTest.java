f5e1d453974b64852a34c86a844a4882217d2cb9
Thomas Perkins <thomasperkins1123@gmail.com>
2019-10-14 11:10:45 +0100

    Update tests from review

--- src/test/java/net/lingala/zip4j/crypto/PBKDF2/BinToolsTest.java
+++ src/test/java/net/lingala/zip4j/crypto/PBKDF2/BinToolsTest.java
@@ -1,65 +1,52 @@
 package net.lingala.zip4j.crypto.PBKDF2;
 
-import java.lang.reflect.Array;
-import net.lingala.zip4j.crypto.PBKDF2.BinTools;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.junit.rules.Timeout;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BinToolsTest {
-  @Rule
-  public final Timeout globalTimeout = new Timeout(10000);
 
   @Rule
   public final ExpectedException thrown = ExpectedException.none();
 
   @Test
-  public void bin2hexInput1OutputNotNull2() {
+  public void bin2hexForValidInputReturnsValidHex() {
     final byte[] b = {(byte) 112};
-    Assert.assertEquals("70", BinTools.bin2hex(b));
+    assertThat(BinTools.bin2hex(b)).isEqualTo("70");
   }
 
   @Test
-  public void bin2hexInput0OutputNotNull() {
-    final byte[] b = {};
-    Assert.assertEquals("", BinTools.bin2hex(b));
+  public void bin2hexForEmptyInputReturnsEmptyString() {
+    assertThat(BinTools.bin2hex(new byte[]{})).isEqualTo("");
   }
 
   @Test
-  public void bin2hexInputNullOutputNotNull() {
-    Assert.assertEquals("", BinTools.bin2hex(null));
+  public void bin2hexForNullInputReturnsEmptyString() {
+    assertThat(BinTools.bin2hex(null)).isEqualTo("");
   }
 
   @Test
-  public void hex2binInputNullOutput0() {
+  public void bin2hexForNullInputReturnsEmptyArray() {
     final String s = null;
-    final byte[] actual = BinTools.hex2bin(s);
-    Assert.assertArrayEquals(new byte[]{}, actual);
+    assertThat(BinTools.hex2bin(s)).isEqualTo(new byte[]{});
   }
 
   @Test
-  public void hex2binInputNotNullOutputIllegalArgumentException3() {
-    final String s = "foo";
+  public void hex2binForInvalidInputOutputIllegalArgumentException() {
     thrown.expect(IllegalArgumentException.class);
-    BinTools.hex2bin(s);
+    BinTools.hex2bin("foo");
   }
 
   @Test
-  public void hex2binInputAOutputPositive() {
-    Assert.assertEquals(10, BinTools.hex2bin('A'));
+  public void hex2binCharacterInputOutputPositive() {
+    assertThat(BinTools.hex2bin('A')).isEqualTo(10);
   }
 
   @Test
-  public void hex2binInputNotNullOutputIllegalArgumentException2() {
+  public void hex2binInvalidInputOutputIllegalArgumentException() {
     thrown.expect(IllegalArgumentException.class);
     BinTools.hex2bin('\u013c');
-  }
-
-  @Test
-  public void hex2binInputNotNullOutputIllegalArgumentException() {
-    thrown.expect(IllegalArgumentException.class);
-    BinTools.hex2bin('\u0018');
   }
 }
