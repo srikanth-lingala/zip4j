68034dabc011712cad3be6980543926627cae2c0
Anthony Christensen <706087+aljchristensen@users.noreply.github.com>
2020-05-26 11:57:34 -0400

    Add unixMode to ZipParameters copy constructor

--- src/main/java/net/lingala/zip4j/model/ZipParameters.java
+++ src/main/java/net/lingala/zip4j/model/ZipParameters.java
@@ -101,6 +101,7 @@ public class ZipParameters {
     this.rootFolderNameInZip = zipParameters.getRootFolderNameInZip();
     this.fileComment = zipParameters.getFileComment();
     this.symbolicLinkAction = zipParameters.getSymbolicLinkAction();
+    this.unixMode = zipParameters.isUnixMode();
   }
 
   /**
