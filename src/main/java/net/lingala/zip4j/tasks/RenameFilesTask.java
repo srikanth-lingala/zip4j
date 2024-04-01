44a8be7ec237487b706a5413eb8cb6d409f64a18
user <user@MSI.>
2024-04-01 15:37:32 +0200

    fix fileheader match for renaming

--- src/main/java/net/lingala/zip4j/tasks/RenameFilesTask.java
+++ src/main/java/net/lingala/zip4j/tasks/RenameFilesTask.java
@@ -125,7 +125,7 @@ public class RenameFilesTask extends AbstractModifyFileTask<RenameFilesTask.Rena
       String> fileNamesMap) {
 
     for (Map.Entry<String, String> fileHeaderToBeRenamed : fileNamesMap.entrySet()) {
-      if (fileHeaderToBeChecked.getFileName().startsWith(fileHeaderToBeRenamed.getKey())) {
+      if (fileHeaderToBeChecked.getFileName().equals(fileHeaderToBeRenamed.getKey())) {
         return fileHeaderToBeRenamed;
       }
     }
